3b4bc5d709cf100c4ce434e8239116cb57b560ca
Niyaj-Kumanali <iamniyazahmad777@gmail.com>
2023-09-22 23:18:39 +0530

    just made some changes in the css and js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const search = document.querySelector('.search');
 const input = document.querySelector('input');
+const btn = document.querySelector('.btn')
 
-search.addEventListener('click', () => {
+btn.addEventListener('click', () => {
     search.classList.toggle('active');
     input.focus();
 })
